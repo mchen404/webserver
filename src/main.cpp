c975d197c64c8a4e55ebc9e1cc2eee5a2adf8998
Michael Chen <Michael@Michael-PC.(none)>
2013-05-15 19:33:13 -0400

    initialize order because console did not catch some messages when initializing objects

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,29 @@
 #include "server.h"
 #include "console.h"   
+#include "threadpool.h"
+#include "settings.h"
 
 int main( int argc, char** argv ){
 
+	// create all component
+	SmartPointer<Settings> settings(new Settings());
 	SmartPointer<Server> server(new Server());
-	server->initialize();
-	server->Start();
-	
 	SmartPointer<Console> console(new Console());
+	SmartPointer<Threadpool> threadpool(new Threadpool());
+	
+	// primary initialize
 	console->initialize();
+	
+	// secondary initialize
+	settings->initialize();
+	server->initialize();
+	threadpool->initialize();
+	
+	// start threads
+	server->Start();
 	console->Start();
 	
+	// wait for all threads to complete
 	server->thread_join();
 	console->thread_join();
 	
